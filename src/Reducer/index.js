bdcd2a9ba48ae7d3641d2a5eed15ba4a58c9a6f4
rohansharma06 <rohansharma251998@gmail.com>
2021-01-24 18:59:50 +0530

    create reducer  to add issue

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -2,31 +2,14 @@ import {
     FETCH_PROJECT,
     ADD_PROJECT,
     VIEW_PROJECT_DETAILS,
-    VIEW_HOME_PAGE
+    VIEW_HOME_PAGE,
+    ADD_ISSUE
 } from '../Actions/actionType';
 
 const initialAuthState = {
     projects:[],
-    showHome: false,
-    displayProject:{
-        name:"Hospital-API",
-        description:"Basic API to keep track and generate reports of patients in Covid-19 time.",
-        author:"Rohan Sharma",
-        issue: [
-            {
-                title:'bug',
-                description:'invalid data',
-                label:['error'],
-                author:'Admin 1',
-            },
-            {
-                title:'bug2',
-                description:'invalid data',
-                label:['error'],
-                author:'Admin 1',
-            }
-        ]
-    }
+    showHome: true,
+    displayProject:{}
 }
 
 export default function auth(state = initialAuthState, action){
@@ -53,6 +36,17 @@ export default function auth(state = initialAuthState, action){
                 ...state,
                 showHome: true,
                 displayProject: {}
+            }
+        case ADD_ISSUE:
+            let data = state.displayProject;
+            data.issue.push(action.data);
+            let allproject = JSON.parse(localStorage.getItem('projects'));
+            allproject.find(project => project.name === data.name).issue.push(action.data);
+            localStorage.setItem('projects', JSON.stringify(allproject));
+            return {
+                ...state,
+                showHome:false,
+                displayProject:data
             }
         default:
             return state;
